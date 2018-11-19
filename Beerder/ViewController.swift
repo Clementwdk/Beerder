1131f525a6ab81b0c2abb755c34ba2b296a71376
Developer <developer@Developers-Mac.local>
2018-11-19 15:44:38 +0100

    Ajout Modelisatation

--- Beerder/ViewController.swift
+++ Beerder/ViewController.swift
@@ -23,3 +23,47 @@ class ViewController: UIViewController {
 
 }
 
+/*
+ 
+ struct Beer{
+var bar: [Bar]
+var brand: Brand
+var type: String
+var power : String
+var note: Int8
+var name: String
+var barCode: String // Pas sur du string
+var commentaire: String
+}
+/*   func AddBar(barMore :Bar,beerthis: Beer)
+ {
+ // beerthis.bar.append(batMore :Bar)
+ 
+ }*/
+
+
+
+struct Bar{
+    var place: String
+    var schedule: String
+    var note : Int8
+    var nom : String
+    var price : Int8
+}
+
+struct Brand{
+    var name : String
+    var place : String
+    var note : Int8
+    var history: String
+}
+
+  var bar1 = Bar(place: "Chatelet", schedule: "18h00", note: 4 , nom: "le 400", price:4)
+ var bar2 = Bar(place: "Bercy", schedule: "19h00", note: 3 , nom: "Le relais", price:7)
+ var brand1 = Brand(name:"BrewDog",place:"Scotland",note:4, history: " Create in 2001")
+ var beer1 = Beer(bar: [bar1,bar2] ,brand: brand1, type :"IPA", power:"7°c", note:3, name:"Brew IPA", barCode:"13543434",commentaire:"Good")
+ 
+ 
+*/
+
+
